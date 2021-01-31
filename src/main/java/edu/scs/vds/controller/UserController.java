2ab1689458bf259b6d613a05f3f5a8e0adabfb8c
Mahmud Hasan <sunno.sakib@gmail.com>
2021-02-01 02:08:43 +0600

    view added WIP

--- src/main/java/edu/scs/vds/controller/UserController.java
+++ src/main/java/edu/scs/vds/controller/UserController.java
@@ -1,4 +1,14 @@
 package edu.scs.vds.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserWebController {
+
+    @GetMapping("/index")
+    public String login() {
+        return "index";
+    }
+
 }
